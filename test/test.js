58928d55d5e801c49d86ee4805b81b6a5d26d90d
Brian Buchalter <bal711@gmail.com>
2014-11-16 22:10:25 -0700

    Implement numeric cases

--- test/test.js
+++ test/test.js
@@ -1,9 +1,28 @@
+function getRelationship(x, y) {
+  if(x > y) {
+    return x + " > " + y;
+  } else if(x < y) {
+    return x + " < " + y;
+  } else {
+    return x + " = " + y;
+  }
+}
+
 var assert = require("assert")
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
+describe("getRelationship(2,3)", function () {
+  it("should return '2 < 3'", function () {
+    assert.equal('2 < 3', getRelationship(2, 3));
+  });
+});
+
+describe("getRelationship(3,2)", function () {
+  it("should return '3 > 2'", function () {
+    assert.equal('3 > 2', getRelationship(3, 2));
+  });
+});
+
+describe("getRelationship(3,3)", function () {
+  it("should return '3 = 3'", function () {
+    assert.equal('3 = 3', getRelationship(3, 3));
+  });
 });
