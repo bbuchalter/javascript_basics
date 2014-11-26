047e62e1ce974f5e3ba01b52d81d1548941086ea
Brian Buchalter <bal711@gmail.com>
2014-11-26 05:40:53 -0700

    Add comments for getRelationship

--- relation_directions.js
+++ relation_directions.js
@@ -1,7 +1,14 @@
+/*
+ Returns a string explaining the reason why the first argument is not a number.
+ */
 function cannotCompareNaN(value) {
   return 'Can\'t compare relationships because ' + value + ' is not a number';
 }
 
+/*
+ Requires two arguments of any type.
+ Returns a string describing the relative values of those two inputs.
+ */
 function getRelationship(x, y) {
   if (isNaN(x) && isNaN(y)) {
     return 'Can\'t compare relationships because ' + x + ' and ' + y + ' are not numbers';
