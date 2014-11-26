3375be8d6a1cf6674f44ea65dbe40e35dd5dcca7
Brian Buchalter <bal711@gmail.com>
2014-11-26 05:38:40 -0700

    Add comments to alphabetizer and extract splitName function

--- alphabetizer.js
+++ alphabetizer.js
@@ -1,14 +1,32 @@
+/*
+Requires a name as a string in the format "First Last" as first argument.
+Returns an Object which responds to "first" and "last".
+ */
+function splitName(name) {
+  var nameObj = {};
+  var nameParts = name.split(" ");
+  nameObj.first = nameParts[0];
+  nameObj.last = nameParts[1];
+  return nameObj;
+}
+
+/*
+Requires an array of names in the format "First Last" as first argument.
+Returns an array of names in the format "Last, First".
+ */
 function formatNames(names) {
   var formattedNames = [];
   for(var i = 0; i < names.length; i++) {
-    var nameParts = names[i].split(" ");
-    var firstName = nameParts[0];
-    var lastName = nameParts[1];
-    formattedNames.push(lastName + ", " + firstName);
+    var name = splitName(names[i]);
+    formattedNames.push(name.last + ", " + name.first);
   }
   return formattedNames;
 }
 
+/*
+ Requires an array of names in the format "First Last" as first argument.
+ Returns an array of names in the format "Last, First" sorted alphabetically.
+ */
 function alphabetizer(names) {
   return formatNames(names).sort();
 }
--- psi_parser.js
+++ psi_parser.js
@@ -1,7 +1,19 @@
+/*
+ Returns an integer of total number of bytes in the request and response including totalRequestBytes, cssResponseBytes,
+ imageResponseBytes, javascriptResponseBytes, and otherResponseBytes.
+ */
 function totalBytes(results) {
-  return parseInt(results.pageStats.totalRequestBytes);
+  return parseInt(results.pageStats.totalRequestBytes) +
+    parseInt(results.pageStats.htmlResponseBytes) +
+    parseInt(results.pageStats.cssResponseBytes) +
+    parseInt(results.pageStats.imageResponseBytes) +
+    parseInt(results.pageStats.javascriptResponseBytes) +
+    parseInt(results.pageStats.otherResponseBytes);
 }
 
+/*
+ Returns an array of the localized rule names PSI suggests you improve.
+ */
 function ruleList(results) {
   var list = [];
   var ruleResults = results.formattedResults.ruleResults;
